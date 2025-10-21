eea34ddf82e5b5203a87af14940b733242e1e380
RomaneFatima-Zahra <barhoufz@gmail.com>
2025-10-21 15:51:03 +0200

    ajout des loggs

--- API_Fastapi.py
+++ API_Fastapi.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field
 from enum import Enum
 import joblib
 import pandas as pd
 import traceback
+import logging
+import os
+import time 
+import uuid 
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+import json
 
 # ============================================================
 # Définition des Enums pour les champs à choix limités
@@ -178,8 +185,8 @@ class ClientData(BaseModel):
     NAME_FAMILY_STATUS: NAME_FAMILY_STATUS
     NAME_HOUSING_TYPE: NAME_HOUSING_TYPE
     REGION_POPULATION_RELATIVE: float
-    DAYS_BIRTH: int
-    DAYS_EMPLOYED: int
+    DAYS_BIRTH: int = Field(le=0)
+    DAYS_EMPLOYED: int = Field(le=0)
     DAYS_REGISTRATION: int
     DAYS_ID_PUBLISH: int
     FLAG_EMP_PHONE: int = Field(ge=0, le=1)
@@ -211,6 +218,30 @@ class ClientData(BaseModel):
     CREDIT_TERM: float
     DAYS_EMPLOYED_PERCENT: float
 
+# ============================================================
+# Configuration du logger
+# ============================================================
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE = os.path.join(LOG_DIR, "api_logger.log")
+
+logger = logging.getLogger("api_logger")
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(LOG_FILE, maxBytes=10_000_000, backupCount=5)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+if not logger.handlers:
+    logger.addHandler(handler)
+
+logger.info("Logs écrits dans logs/api_logger.log dans le dossier du projet.")
+
+#Fonction pour écrire des logs structurés au format JSONL
+
+def write_log(entry: dict):
+    try:
+        logger.info(json.dumps(entry, ensure_ascii=False))
+    except Exception as e : 
+        logger.error(f"Erreur lors de l'écriture du log structuré{e}")
 
 #-----------------------------------------------------------------------------------------------------
 # Création de l'application FastAPI et chargement du modèle
@@ -222,46 +253,109 @@ app = FastAPI(
     version="3.0",
     )
 
+# Middleware de logging
+
+@app.middleware("http")
+async def log_request_middleware(request: Request, call_next):
+    request_id = str(uuid.uuid4())
+    request.state.request_id = request_id
+    start_time = time.time()
+    logger.info(f"Début requête {request_id} : {request.method} {request.url.path}")
+    response = await call_next(request)
+    duration = time.time() - start_time
+    status_code = response.status_code
+    logger.info(f"Fin requête {request_id} : {request.method} {request.url.path} - "
+            f"Status {response.status_code} - Durée {duration:.3f}s")
+    write_log({
+        "timestamp": datetime.utcnow().isoformat(),
+        "request_id": request_id,
+        "method": request.method,
+        "path": request.url.path,
+        "status_code": status_code,
+        "duration": duration,
+        "client_ip": request.client.host if request.client else "unknown",
+        "event": "http_request"
+    })
+
+    return response 
+
+# Chargement du modèle
+
 try:
     model = joblib.load("model.pkl")
-    print("Modèle chargé avec succès.")
+    logger.info("Modèle chargé avec succès.")
 except Exception as e:
+    logger.critical(f"Erreur lors du chargement du modèle : {e}", exc_info=True)
     print(f" Erreur de chargement du modèle : {e}")
     model = None
+
 
 #-----------------------------------------------------------------------------------------------------
 # 1er Endpoint : Route d'accueil qui fournit un message de bienvenue, et oriente vers les endpoints.
 #-----------------------------------------------------------------------------------------------------
 
-@app.get("/", tags=["Accueil"], summary="Page d'accueil", description="Retourne un message de bienvenue et les principaux endpoints de l’API.")
+@app.get("/", tags=["Accueil"], summary="Page d'accueil", description=" un message de bienvenue et les principaux endpoints de l’API.")
 def home():
+    logger.info("Accès à l'endpoint d'accueil")
     return {
     "message": "Bienvenue dans l’API de prédiction de solvabilité des clients",
     "description" : "Cette API utilise un modèle de Machine Learning pour prédire si un client est solvable ou défaillant",
     "endpoints": {
     "documentation": "/docs",
-    "faire une prédiction": "/predict_predict_post" }
+    "faire une prédiction": "/predict" }
     
     }
-
 
 #------------------------------------------------------------------------------------------------------------------
 # 2eme Endpoint : Route de prédiction pour faire une prédiction avec les données saisies. attend une requete POST 
 #------------------------------------------------------------------------------------------------------------------
-@app.post("/predict")
-def predict(client: ClientData):
-    """Endpoint de prédiction principale"""
+
+@app.post("/predict", tags=["Prédiction"], summary="Faire une prédiction", description="Prédit si un client est solvable ou défaillant.")
+
+def predict(request: Request, client: ClientData):
+    "Endpoint de prédiction principale"
+    request_id = getattr(request.state, "request_id", "unknown")
+    logger.info(f"Requête de prédiction reçue - Request ID: {request_id}")
+
     if model is None:
+        logger.critical(f"Modèle non chargé au moment de la prédiction- Request ID: {request_id}")
+        write_log({
+            "timestamp": datetime.utcnow().isoformat(),
+            "request_id": request.state.request_id if hasattr(request.state, "request_id") else "unknown",
+            "event": "prediction_error",
+            "error": "Modèle non chargé"
+        })
         raise HTTPException(status_code=500, detail="Modèle non chargé")
 
     try:
         df = pd.DataFrame([client.dict()])
+        logger.info(f"DataFrame créé avec succès - Shape: {df.shape} - Request ID : {request_id}")
         y_pred = model.predict(df)[0]
         y_proba = model.predict_proba(df)[0][1]
+        prediction = "Défaillant" if y_pred == 1 else "Solvable"
+        probabilité_defaut = round(float(y_proba), 4)
+
+        logger.info(f"Prédiction calculée : {prediction} - Probabilité de défaut : {probabilité_defaut}")
+        write_log({
+            "timestamp": datetime.utcnow().isoformat(),
+            "request_id": request.state.request_id,
+            "event": "prediction",
+            "input_data": client.dict(),
+            "prediction": prediction,
+            "probabilité_defaut": probabilité_defaut
+        })
         return {
-            "prediction": "Défaillant" if y_pred == 1 else "Solvable",
-            "probabilité_defaut": round(float(y_proba), 4)
+            "prediction": prediction,
+            "probabilité_defaut": probabilité_defaut
         }
     except Exception as e:
+        logger.error(f"Erreur lors de la prédiction - Request ID : {request_id}", exc_info=True)
         print(traceback.format_exc())
-        raise HTTPException(status_code=400, detail=str(e))
+        write_log({
+            "timestamp": datetime.utcnow().isoformat(),
+            "request_id": request.state.request_id if hasattr(request.state, "request_id") else "unknown",
+            "event": "prediction_error",
+            "error": str(e),
+            "traceback": traceback.format_exc()
+        })
+        raise HTTPException(status_code=400, detail="Erreur lors de la prédiction. Vérifiez les données d'entrée.")
